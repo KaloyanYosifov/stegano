6f0d0d4f2581f6e9017aac3e2cc45dc09ebfdef3
KaloyanYosifov <kaloqn665@gmail.com>
2023-06-09 22:42:15 +0200

    Finally implement encryption

--- stegano-core/src/crypto.rs
+++ stegano-core/src/crypto.rs
@@ -1,5 +1,5 @@
 use aes_gcm::{
-    aead::{Aead, AeadCore, KeyInit, OsRng},
+    aead::{rand_core::RngCore, Aead, AeadCore, KeyInit, OsRng},
     Aes256Gcm,
 };
 use argon2::Argon2;
@@ -7,41 +7,48 @@ use argon2::Argon2;
 use crate::{Result, SteganoError};
 
 const NONCE_LEN: usize = 12;
+const SALT_LEN: usize = 16;
+const TOTAL_META_LEN: usize = NONCE_LEN + SALT_LEN;
 
-pub fn derive_key(password: &str) -> Result<Vec<u8>> {
+pub fn derive_key(password: &str, salt: &[u8]) -> Result<Vec<u8>> {
     let mut key = [0; 32];
 
-    match Argon2::default().hash_password_into(
-        password.as_bytes(),
-        b"salting is ok right?",
-        &mut key,
-    ) {
+    match Argon2::default().hash_password_into(password.as_bytes(), &salt, &mut key) {
         Ok(_) => Ok(key.to_vec()),
         _ => Err(SteganoError::CannotEncryptData),
     }
 }
 
 pub fn encrypt(message: &str, password: &str) -> Result<Vec<u8>> {
-    let key = derive_key(password)?;
+    let mut salt = [0u8; SALT_LEN];
+    OsRng.fill_bytes(&mut salt);
+
+    let key = derive_key(password, &salt)?;
     let cipher = Aes256Gcm::new((&key[..]).into());
     let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+
+    assert_eq!(NONCE_LEN, nonce.len());
+
     let ciphertext = cipher.encrypt(&nonce, message.as_bytes()).unwrap();
     let ciphertext_len = ciphertext.len();
-    let mut ciphertext2: Vec<u8> = Vec::with_capacity(ciphertext_len + NONCE_LEN);
+    let new_ciphertext_len = ciphertext_len + TOTAL_META_LEN;
+    let mut ciphertext2: Vec<u8> = Vec::with_capacity(new_ciphertext_len);
 
-    ciphertext2.resize(ciphertext_len + NONCE_LEN, 0);
+    ciphertext2.resize(new_ciphertext_len, 0);
     ciphertext2[..NONCE_LEN].copy_from_slice(&nonce);
-    ciphertext2[NONCE_LEN..].copy_from_slice(&ciphertext);
+    ciphertext2[NONCE_LEN..TOTAL_META_LEN].copy_from_slice(&salt);
+    ciphertext2[TOTAL_META_LEN..].copy_from_slice(&ciphertext);
 
     Ok(ciphertext2)
 }
 
 pub fn decrypt(ciphertext: &[u8], password: &str) -> Result<String> {
-    let key = derive_key(password)?;
+    let nonce = &ciphertext[0..NONCE_LEN];
+    let salt = &ciphertext[NONCE_LEN..TOTAL_META_LEN];
+    let actual_cipher_text = &ciphertext[TOTAL_META_LEN..];
+    let key = derive_key(password, salt)?;
     let cipher = Aes256Gcm::new((&key[..]).into());
-    let nonce = &ciphertext[0..12];
-    let ciphertext = &ciphertext[12..];
-    let decrypted = cipher.decrypt(nonce.into(), ciphertext).unwrap();
+    let decrypted = cipher.decrypt(nonce.into(), actual_cipher_text).unwrap();
 
     Ok(std::str::from_utf8(&decrypted[..]).unwrap().to_string())
 }
